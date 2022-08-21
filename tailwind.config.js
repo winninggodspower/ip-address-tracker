2c872987f0f622ad09256ac42fc9d5315d9b3811
winning godspower <winninggodspower@gmail.com>
2022-08-21 17:38:34 +0100

    done with design

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./images/pattern-bg.png')",
+      },
+      colors: {
+        'v-dark-grey': 'hsl(0, 0%, 17%)',
+        'dark-grey': 'hsl(0, 0%, 59%)'
+      },
+    },
+  },
+  plugins: [],
+}
